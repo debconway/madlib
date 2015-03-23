b04b7059a09776376a0b48a96681f5cdbe9e46cc
vibbs <vibbs@github.com>
2015-03-23 17:16:33 -0400

    added output

--- js/madlib.js
+++ js/madlib.js
@@ -4,13 +4,19 @@ $(document).ready(function() {
 
 		console.log("Hello to the log world");
 
-		var exclamation = document.getElementById("exclamation");
-		var adverb = document.getElementById("adverb");
-		var noun = document.getElementById("noun");
-		var verb = document.getElementById("verb");
+		//var exclamation = document.getElementById("exclamation").value();
+		var exclamation = $('#exclamation').val();
+		var adverb = $('#adverb').val();
+		var noun = $('#noun').val();
+		var verb = $('#verb').val();
 
 		console.log(exclamation);
 
+		var mad_lib = "'" + exclamation + "!' he said, as he " + adverb + " jumped onto his " + noun + " and " + verb + " into the sunset.";
+
+		$("#para").html("<h1>"+mad_lib+"</h1>");
+
+		console.log(mad_lib);
 
 	});
 });
