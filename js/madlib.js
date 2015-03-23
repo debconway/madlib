5ee087fb25fd792b975f35a2d02246ed66c8d30a
vibbs <vibbs@github.com>
2015-03-23 17:02:33 -0400

    added the wroking jd button fucntion

---
+++ js/madlib.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+	$("#submit-btn").click(function(){
+
+		console.log("Hello to the log world");
+
+		var exclamation = document.getElementById("exclamation");
+		var adverb = document.getElementById("adverb");
+		var noun = document.getElementById("noun");
+		var verb = document.getElementById("verb");
+
+		console.log(exclamation);
+
+
+	});
+});
